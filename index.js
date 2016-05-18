07a2da6c66cef11659774e43edd97293b02c4c1a
Paul Maslany <prmaslany@Pauls-MacBook-Pro.local>
2016-05-18 13:25:30 -0400

    we are going to use superagent for http calls to Nota

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var argv = require('minimist')(process.argv.slice(2));
-console.log("Parsing command line arguments....");
+//console.log("Parsing command line arguments....");
 console.log(argv);
+//console.log(argv.u);
+//console.log("Finished Parsing command line arguments...."); 
+if (argv.h) {
+	console.log("-u=<filename>");
+}
